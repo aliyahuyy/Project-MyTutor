d8d9035ce37216507aa5d07520906ba5a1f56061
aqilah0wl <aqilahfitri@student.telkomuniversity.ac.id>
2021-06-29 00:17:41 +0800

    Buat project
    Commit 1

--- FormPembayaran/FormPembayaran.Designer.cs
+++ FormPembayaran/FormPembayaran.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
-namespace setUP_main
+namespace FormPembayaran
 {
-    partial class Form1
+    partial class FormPembayaran
     {
         /// <summary>
         /// Required designer variable.
@@ -29,21 +29,34 @@ namespace setUP_main
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(631, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Bayar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormPembayaran
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.Controls.Add(this.button1);
+            this.Name = "FormPembayaran";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.FormPembayaran_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
     }
 }
 
