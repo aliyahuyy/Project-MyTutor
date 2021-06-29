9a5c2c93143ac5319e5ac7e54eed7ef5c30325a2
aqilah0wl <aqilahfitri@student.telkomuniversity.ac.id>
2021-06-29 09:15:20 +0800

    Form Pembayaran
    Form Pembayaran 1.0

--- FormPembayaran/FormPembayaran.cs
+++ FormPembayaran/FormPembayaran.cs
@@ -31,5 +31,15 @@ namespace FormPembayaran
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
