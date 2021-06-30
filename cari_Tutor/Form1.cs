bd76c1c1b319c5752aa0ef2d4a21d77cd41d69d0
faliaaufa <55497068+faliaaufa@users.noreply.github.com>
2021-06-30 08:44:53 +0700

    Update daftar tutor
    file setup main dihapus

--- cari_Tutor/Form1.cs
+++ cari_Tutor/Form1.cs
@@ -15,18 +15,24 @@ namespace cari_Tutor
     public Form1()
         {
             InitializeComponent();
-
+            
             foreach (string name_ in Enum.GetNames(typeof(Daftar_Tutor.Nama_tutor)))
             {
                 tb_daftar_tutor.Rows.Add(name_, Daftar_Tutor.GetMapel(name_), Daftar_Tutor.GetTingPed(name_),
                     Daftar_Tutor.GetHarga(name_), Daftar_Tutor.GetKota(name_));
+ 
             }
             
         }
 
         private void tb_daftar_tutor_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            if (tb_daftar_tutor.CurrentRow.Selected == true)
+            {
+                button1.Enabled = true;
+            }
+            else { button1.Enabled = false; }
+            
         }
     }
 }
